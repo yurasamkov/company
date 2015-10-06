f0719a882a91e2818c17af08bbd1e0da23ac9448
Yura Samkov <y.samkoff@ukr.net>
2015-10-06 13:27:04 +0300

    add to user callback

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,18 +1,5 @@
 class Profile < ActiveRecord::Base
 	belongs_to :user
 
-   
-private
-
-def create
-    @user = User.new(user_params)
-    if @user.save
-        @profile = Profile.create
-        @profile.user_id = @user.id
-        @profile.save
-        redirect_to root_url, :notice => "You have succesfully signed up!"
-    else
-        render "new"
-    end
-
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,29 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_one :profile, dependent: :destroy
+  has_one :profile, dependent: :destroy
 
- #after_create: create_profile
+  after_create :user_id_created
 
+  private
+
+  def user_id_created
+   @pofile = Profile.new 
+  self.profile.user_id = self.current_user.id
+       self.profile.save
+   
+  end
 
 end
+
+#@pofile = @profile.user_id
+ #  @pofile.save
+#@comment = @post.comments.build(params[:comment])
+#@comment.user_id = current_user.id
+#@comment.save
+
+ #   @pofile.user_id = current_user.id
+ #   @user_id.save
+
+ #self.post.last_commented_at = self.created_at
+       #self.post.save
