b2d3ce6b43d69f9e351666f4d89db1af711bdfba
Yura Samkov <y.samkoff@ukr.net>
2015-10-06 13:42:08 +0300

    add to user callback 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,21 +12,9 @@ class User < ActiveRecord::Base
 
   def user_id_created
    @pofile = Profile.new 
-  self.profile.user_id = self.current_user.id
-       self.profile.save
+   @profile.user_id = self.id  
+   @profile.save
    
   end
 
 end
-
-#@pofile = @profile.user_id
- #  @pofile.save
-#@comment = @post.comments.build(params[:comment])
-#@comment.user_id = current_user.id
-#@comment.save
-
- #   @pofile.user_id = current_user.id
- #   @user_id.save
-
- #self.post.last_commented_at = self.created_at
-       #self.post.save
