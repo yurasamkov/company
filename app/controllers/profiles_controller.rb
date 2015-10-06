8a0adabc7063ddb96f7859749b4101b32131e81a
Yura Samkov <y.samkoff@ukr.net>
2015-10-06 15:14:50 +0300

    link to my_profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,11 @@
+class ProfilesController < ApplicationController
+	
+	respond_to :html
+
+	def edit
+    end
+
+    def my_profile
+    	
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Company::Application.routes.draw do
   
   devise_for :users
   root  'pages#home'
+  resources :profiles
+  #get "profiles/my_profiles" 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
