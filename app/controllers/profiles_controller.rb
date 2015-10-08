dfd489c54bbbf927f1057d1e35afb8d94aaeb759
Yura Samkov <y.samkoff@ukr.net>
2015-10-08 23:31:19 +0300

    My_profile/edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,10 +2,43 @@ class ProfilesController < ApplicationController
 	
 	respond_to :html
 
-	def edit
+  def index
+   
   end
 
   def show
+    @profile = Profile.find(params[:id])
   end
 
+  def new
+    @profile = Profile.new
+  end
+
+  def edit
+  end
+
+  def create
+   @profile = Profile.new(profile_params)
+    if @profile.save
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    
+  end
+
+  def destroy
+   
+  end
+
+
+  private
+
+    def profile_params
+      params.require(:profile).permit(:phone, :city, :country, :birth_date,
+                                      :profession, :education, :image)
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Company::Application.routes.draw do
   devise_for :users
   root  'pages#home'
   get '/my_profile', to: 'profiles#edit'
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
